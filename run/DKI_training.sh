17242955137737ccdf5223bebfb2546a7760d536
Chenyi Jiang <71124351+LanchJL@users.noreply.github.com>
2024-04-29 17:59:42 +0800

    Add files via upload

--- run/DKI_training.sh
+++ run/DKI_training.sh
@@ -1,8 +1,10 @@
 python3 ../DKI_training.py \
 --dataset cityscapes \
---data_root "/media/yujiaguo/29719e14-8bb8-4829-90a4-727c2e661fc4/JCY/dataset/CITYSPACES/" \
+--data_root "" \
 --resize_feat \
 --batch_size 16 \
---lambda_s 1.25 \
---DKI_save_dir "../VAE_weights_GTA5/" \
+--lambda_s 1.0 \
+--DKI_save_dir "" \
+--target_domain snow \
+--training_epochs 50 \
 --train
---
+++ run/ELP&SCC.sh
@@ -0,0 +1,8 @@
+python3 ../ELP_SCC.py \
+--dataset cityscapes \
+--data_root "" \
+--resize_feat \
+--batch_size 16 \
+--DKI_checkpoints "" \
+--Style_dir ""
+
