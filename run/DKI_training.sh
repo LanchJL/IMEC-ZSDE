43362358f9ce321bd3444d6d30463c58251e0031
Chenyi Jiang <71124351+LanchJL@users.noreply.github.com>
2024-07-22 17:12:57 +0800

    Add files via upload

--- run/DKI_training.sh
+++ run/DKI_training.sh
@@ -1,10 +1,11 @@
 python3 ../DKI_training.py \
 --dataset cityscapes \
---data_root "" \
+--data_root "./dataset/ZSDE/city" \
 --resize_feat \
---batch_size 16 \
---lambda_s 1.0 \
---DKI_save_dir "" \
---target_domain snow \
---training_epochs 50 \
+--batch_size 64 \
+--lambda_s 0.1 \
+--DKI_save_dir "../DKI_weights/CS2Night/" \
+--target_domain night \
+--training_epochs 200 \
+--gpu_id 2 \
 --train
--- run/ELP&SCC.sh
+++ run/ELP&SCC.sh
@@ -1,8 +1,17 @@
 python3 ../ELP_SCC.py \
 --dataset cityscapes \
---data_root "" \
+--data_root "./dataset/ZSDE/city" \
 --resize_feat \
 --batch_size 16 \
---DKI_checkpoints "" \
---Style_dir ""
+--DKI_checkpoints "../DKI_weights/CS2Snow/DKI.pth" \
+--Style_dir "../Styles/CS2Snow2/" \
+--Style_dir2 "../Styles/CS2SnowSCC3/" \
+--alpha_s 0.05 \
+--l 20 \
+--SCC \
+--ELP \
+--target_domain snow \
+--gpu_id 0 \
+
+
 
